fbaf804a2bdfc75f2c5078c78c314be15fd875a8
BTL86 <phamlehuy52@gmail.com>
2022-10-13 13:59:11 +0700

    clear

--- MyWebApiApp/Controllers/ReportController.cs
+++ MyWebApiApp/Controllers/ReportController.cs
@@ -14,7 +14,7 @@ namespace AMGAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class ReportController : ControllerBase
     {
 
